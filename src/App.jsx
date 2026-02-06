fa45a9573d84306b2ee08869eeb117f27a8b27aa
kaleemullah <kaleem@arrowhead.pk>
2026-02-06 23:51:13 +0500

    Adding one image and adding react-router-dom for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Dashboard } from './layouts/Dashboard'
 function App() {
 
   return (
     <>
-      
+      <Dashboard />
       <div className="container grid grid-cols-2 justify-center items-center h-screen">
 
         <div className="card bg-white rounded-lg shadow-lg">
@@ -13,6 +14,7 @@ function App() {
         <div className="card bg-white rounded-lg shadow-lg"> Second </div>
 
       </div>
+      
 
     </>
   )
---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,6 @@
+export const Dashboard = () => {
+  return (
+    <div> 
+        <h1 className="">Dashboard</h1>
+    </div>
+  )}
