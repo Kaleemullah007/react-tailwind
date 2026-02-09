5db295f770e3d49089f953b426d4d3e88c0953fb
kaleemullah <kaleem@arrowhead.pk>
2026-02-09 23:59:39 +0500

    Added comment

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export const PublicRoutes = () => {
   const token = localStorage.getItem("token");
-
+ // Perfect place to add a loading state if you want to check token validity with an API call
   // If user is logged in, block public pages
   if (token) {
     return <Navigate to="/" replace />;
