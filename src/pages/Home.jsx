55b1486ac73f2f55b9a3daf070c63d228af25f4a
kaleemullah <kaleem@arrowhead.pk>
2026-02-10 19:46:38 +0500

    Change text label and add naviation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <>
-    <p className="flex text-4xl text-center justify-center items-center h-screen">Welcome to Home Page</p>
+    <p className="flex text-4xl text-center justify-center items-center h-screen" >Welcome to Home Page</p>
   </> 
     )
 }
