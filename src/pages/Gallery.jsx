86608d1e5614fc3dd9d13c37bcc3e9c253c6826d
kaleemullah <kaleem@arrowhead.pk>
2026-02-10 19:45:52 +0500

    Added Galary page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,7 @@
 export const Gallery = () => {
   return (
     <>
-    <p className="flex text-4xl text-center justify-center items-center h-screen">Welcome to Gallery Page</p>
+    <p className="flex text-4xl text-center justify-center items-center h-screen ">Welcome to Gallery Page</p>
   </> 
     )
 }
