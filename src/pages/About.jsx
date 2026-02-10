46618bd7f34ebe00786c9e47d300b2c3e48db91b
kaleemullah <kaleem@arrowhead.pk>
2026-02-10 19:46:16 +0500

    Added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return (
     <>
-    <p className="flex text-4xl text-center justify-center items-center h-screen">Welcome to About Page</p>
+    <p className="flex text-4xl text-center justify-center items-center h-screen ">Welcome to About Page</p>
   </> 
     )
 }
